83ed00eb4573cb9d25368cf697b899e4faf11b95
figueiredosoftware <figueiredosoftware@gmail.com>
2021-01-18 21:14:45 -0300

    commit 20210118

---
+++ Agenda de Compromissos/Agenda de Compromissos/Controllers/TesteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenda_de_Compromissos.Controllers
+{
+    public class TesteController : Controller
+    {
+        public string Metodo01()
+        {
+            return "Teste MVC : " + DateTime.Now.ToLongDateString();
+        }
+
+        public ViewResult Metodo02()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Agenda de Compromissos/Agenda de Compromissos/Global.asax.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing; //Classe RouteTable (adicionado)
+using System.Web.Mvc; //Método de Extensão MapRoute (adicionado)
+
+
+namespace Agenda_de_Compromissos
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //
+            // Cria um roteamento:
+            // http://servidor/classeController/metodo
+            // exemplo:
+            // http://servidor/Produtos/ListagemGeral
+            //
+            RouteTable.Routes.MapRoute("Roteamento", "{controller}/{action}");
+            //Outro roteamento
+            RouteTable.Routes.MapRoute("RoteamentoComParametro", "{controller}/{action}/{id}");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
