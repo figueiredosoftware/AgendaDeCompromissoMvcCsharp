56420cf127e5c70bf32962bbaa883d92120454a3
figueiredosoftware <figueiredosoftware@gmail.com>
2021-02-08 22:11:57 -0300

    commit 08/02/2021

---
+++ Agenda de Compromissos/Agenda de Compromissos/App_Helpers/RotinasWeb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenda_de_Compromissos.App_Helpers
+{
+    public class RotinasWeb
+    {
+        public static DateTime dateTimeStringParaDateTime(string data, string hora)
+        {
+            if(string.IsNullOrEmpty(data) || string.IsNullOrEmpty(hora))
+            {
+                return DateTime.MinValue;
+            }
+
+            DateTime d;
+            if(DateTime.TryParse(data + ' ' + hora, out d))
+            {
+                return d;
+            }
+            else
+            {
+                return DateTime.MinValue;
+            }
+        }
+    }
+}
